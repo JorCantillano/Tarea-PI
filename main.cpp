8f08fa368b2fd067b894ba174da11dc779db2f8a
Jordan <jordancan9@gmail.com>
2019-02-13 23:05:57 -0600

    Enunciado  y funcion de Taylor

--- main.cpp
+++ main.cpp
@@ -3,8 +3,10 @@
 #include <math.h>
 #include <iomanip>
 
-int main(){
-	int count=50*20000;
+
+double calcularPi(int UT){
+	//UT: Unidad de trabajo
+	int count=50*UT;
 	double sum = 0;
 	for (int i = 0; i < count; ++i){
 		if (i & 0x1)
@@ -14,7 +16,15 @@ int main(){
 		else{
 			sum += 1.0/(2*i+1);
 		}
-	
 	}
-	std:: cout<< std::setprecision(15)<<(4*sum) << std::endl;
 }
+
+int main(){
+	double countt=calcularPi(10000);
+	
+	std:: cout<< std::setprecision(15)<<(4*countt) << std::endl;
+	return 0;
+
+}
+
+
